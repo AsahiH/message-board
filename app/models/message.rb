5d20571474cbbcb7a31abb6e49c957c44427c717
Asahi Hatta <asahi-aus-hawaii@live.com>
2016-12-19 07:19:56 +0000

    age column added Lesson 6, challenge2

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,8 @@
 class Message < ActiveRecord::Base
     #　名前は必須入力かつ２０文字以内
-    validates :name , length: {maximum: 20 } , presence: true
+    validates :name , length: { maximum: 20 } , presence: true
     #　内容は必須入力かつ２文字以上３０文字以内
     validates :body , length: { minimum: 2, maximum: 30 } , presence: true
+    #　年齢は０歳以上１５０歳以下
+    validates :age , numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 150 } , presence: true 
 end
-
